1bd10603608d619618a4154ca8c688c4ab50eba2
Thomas <thomaschou861205@gmail.com>
2018-04-01 16:37:44 +0800

    complete ver

--- mazeSolver.cpp
+++ mazeSolver.cpp
@@ -13,6 +13,9 @@ struct offset{
 	int _x;
 	int _y;
 };
+namespace my{
+	struct offset move[8];
+}
 
 typedef struct step{
 	int x; 
@@ -21,68 +24,16 @@ typedef struct step{
 } STEP;
 
 void initMove(struct offset *);
-bool checkBorder(int, int);
+void initMaze(vector< vector<int> > &, int, int); // height, length
+bool checkBorder(int, int); // x, y
+void mazeSolver(vector< vector<int> > &);
 
 int main(){
-	struct offset move[8];
-	stack<STEP> steps;
 	vector< vector<int> > row(MAZE_LENGTH, vector<int>(MAZE_HEIGHT, 0)); // maze
-	vector< vector<bool> > mark(MAZE_LENGTH, vector<bool>(MAZE_HEIGHT, false));
-	int dest_x = 10;
-	int dest_y = 7;
-	bool has_path = false;
 
-	initMove(move);
-
-	for (int i = 0; i < MAZE_HEIGHT; i++){
-		for (int j = 0; j < MAZE_LENGTH; j++)
-			cin >> row.at(j).at(i);
-	}
-
-	// for (int i = 0; i < MAZE_HEIGHT; i++){
-	// 	for (int j = 0; j < MAZE_LENGTH; j++){
-	// 		if (!row.at(j).at(i)) cout << row.at(j).at(i);
-	// 		else cout << " ";
-	// 	}
-	// 	cout << endl;
-	// }
-
-	STEP tmp = {0, 1, E};
-	steps.push(tmp);
-	mark.at(0).at(1) = true;
-
-	while (!steps.empty()){
-		tmp = steps.top();
-		steps.pop();
-		int i = tmp.x; int j = tmp.y; int d = tmp.dir;
-		cout << endl << "(" << i << "," << j << ")->" << d << endl;
-		while (d < 8){
-
-			int g = i + move[d]._x;
-			int h = j + move[d]._y;
-			cout << "(" << g << "," << h << "):" << row.at(g).at(h) << endl;
-
-			if (g == dest_x && h == dest_y){
-				has_path = true;
-				break;
-			}
-			else if (checkBorder(g, h) && !row.at(g).at(h) && !mark.at(g).at(h)){
-				mark.at(g).at(h) = true;
-
-				tmp.x = i; tmp.y = j; tmp.dir = d + 1;
-				steps.push(tmp);
-
-				i = g; j = h; d = N;
-			}
-			else
-				d++;
-		}
-		if (has_path){
-			cout << "There's a path." << endl;
-			break;
-		}
-	}
-	if (!has_path) cout << "There's no path." << endl;
+	initMove(my::move);
+	initMaze(row, MAZE_HEIGHT, MAZE_LENGTH);
+	mazeSolver(row);
 	return 0;
 }
 
@@ -97,8 +48,53 @@ void initMove(struct offset * move){
 	move[NW]._x = -1; move[NW]._y = 1;
 }
 
+void initMaze(vector< vector<int> > &row, int height, int length){
+	for (int i = 0; i < height; i++){
+		for (int j = 0; j < length; j++)
+			cin >> row.at(j).at(i);
+	}
+}
+
 bool checkBorder(int x, int y){
 	if (x >= MAZE_LENGTH || x < 0 || y >= MAZE_HEIGHT || y < 0)
 		return false;
 	else return true;
+}
+
+void mazeSolver(vector< vector<int> > &row){
+	stack<STEP> steps;
+	vector< vector<bool> > mark(MAZE_LENGTH, vector<bool>(MAZE_HEIGHT, false));
+	int dest_x = 10;
+	int dest_y = 7;
+
+	STEP tmp = {0, 1, E};
+	steps.push(tmp);
+	mark.at(0).at(1) = true;
+
+	while (!steps.empty()){
+		tmp = steps.top();
+		steps.pop();
+		int i = tmp.x; int j = tmp.y; int d = tmp.dir;
+		while (d < 8){
+
+			int g = i + my::move[d]._x;
+			int h = j + my::move[d]._y;
+
+			if (g == dest_x && h == dest_y){
+				cout << "There's a path." << endl;
+				return;
+			}
+			else if (checkBorder(g, h) && !row.at(g).at(h) && !mark.at(g).at(h)){
+				mark.at(g).at(h) = true;
+
+				tmp.x = i; tmp.y = j; tmp.dir = d + 1;
+				steps.push(tmp);
+
+				i = g; j = h; d = N;
+			}
+			else
+				d++;
+		}
+	}
+	cout << "There's no path." << endl;
 }
